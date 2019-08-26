f76218a8bbda3bc85d5deff02df904eaf004a0f0
Sindre Sogge Heggen <sindre.heggen@gmail.com>
2019-08-26 18:20:34 +0200

    nrg

--- src/components/start.js
+++ src/components/start.js
@@ -1,5 +1,5 @@
 import React from 'react'
-//import styles from "./menu.module.css"
+import styles from "./menu.module.css"
 import logoNRG from "../images/treev.png" // Tell Webpack this JS file uses this image
 console.log(logoNRG)
 
@@ -9,13 +9,21 @@ const Start = () => (
 forståelsen, mens GeoGebra-delen tar for seg de viktigste verktøyene programvaren tilbyr for å kunne løse problemer knyttet 
 til faget digitalt. Begge delene kan du laste ned fra <i> Filer</i> i menyvalget.</p>
 
+<div className={styles.qt}>
+<q>(...) Jeg tok R2 mens jeg jobbet i Forsvaret, og ved hjelp av Før Kalkulus ble karakteren for å kvalifisere til SIVING studier på NTNU spikret. Læreverket fremstår som meget oversiktlig og er skrevet på en slik måte at det er lett å forstå emnene. </q>
+<p className={styles.qtt}><i>- Påtroppende student</i></p>
+
+<p>
+<img src={logoNRG} alt="logoNRG" style={{width: '70px', verticalAlign: 'middle'}}/></p>
+<p className={styles.nrgt}>
+<a style={{textDecoration: 'none'}} href="https://nrg.no"> Norges Realfagsgymnas</a> bruker Før Kalkulus i sin undervisning</p>
+</div>
+
 
 </div>
 
+
 /**
-<div className={styles.qt}>
-<q>(...) Jeg tok R2 mens jeg jobbet i Forsvaret, og ved hjelp av Før Kalkulus ble karakteren for å kvalifisere til SIVING studier på NTNU spikret. Læreverket fremstår som meget oversiktlig og er skrevet på en slik måte at det er lett å forstå emnene. </q>
-<p className={styles.qtt}>Student (20)</p>
 <p>
 <img src={logoNRG} alt="logoNRG" style={{width: '70px', verticalAlign: 'middle'}}/>&nbsp; Norges Realfagsgymnas bruker Før Kalkulus i sin undervisning</p>
 </div>

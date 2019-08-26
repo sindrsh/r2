0dfde68098b9febaba0b25bf1d98275c961e23ff
sindrsh <sindre.heggen@gmail.com>
2019-08-26 18:29:55 +0200

    Update fil.js

--- src/components/fil.js
+++ src/components/fil.js
@@ -5,8 +5,8 @@ const Fil = () => (
 <div>
 <ul className={styles.atch}>
  <li><a href="https://drive.google.com/open?id=1Cs5Xvn86hxTZpd8iM_T0hOVDFIrAPfE-"> Teoridel</a></li>
- <li><a href="https://drive.google.com/open?id=1Cs5Xvn86hxTZpd8iM_T0hOVDFIrAPfE-"> GeoGebra 5 i R2</a></li>
- <li><a href="https://drive.google.com/open?id=1Cs5Xvn86hxTZpd8iM_T0hOVDFIrAPfE-"> Løsningsforslag; Teoridel</a></li>
+ <li><a href="https://drive.google.com/open?id=1Ydh__umD-p3oZPEpwu6oW3eG4IgWVFYx"> GeoGebra 5 i R2</a></li>
+ <li><a href="https://drive.google.com/open?id=1GQWTLt-0cU5gvO_FbtY3nLY8xPdmFbVX"> Løsningsforslag; Teoridel</a></li>
 </ul>
 </div>
 )
